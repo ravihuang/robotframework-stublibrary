0e725239f003e6ed0bd9a264ea161f49d6dc3793
ravihuang <ravi.huang@gmail.com>
2018-04-24 12:50:07 +0800

    2to3:string_types

--- src/StubLibrary/http_stub.py
+++ src/StubLibrary/http_stub.py
@@ -14,6 +14,7 @@ from .endpoint import Endpoint
 from .statistic import Statistic, RequestedParams
 from .robotlibcore import keyword
 import json as json_reader
+from six import string_types
 
 class HTTP(falcon.API):
     """
@@ -129,7 +130,7 @@ class HTTP(falcon.API):
                  headers = None, cookies = None,
                  json = None):
         '''add response to path with method'''
-        if json and isinstance(json,basestring):
+        if json and isinstance(json,string_types):
             json=json_reader.loads(json)
         self._on_(method,path).response(status,body,content_type,headers,cookies,json)
 
